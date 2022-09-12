6543a288c660cbfcefdc820f6ab1ce0c4553c8cf
Bakytzhan Shymkentbay <bakytzhan.shymkentbay@gmail.com>
2022-09-12 18:23:37 +0600

    Add project files.

---
+++ solid.entity/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid.entity
+{
+    public class Person
+    {
+        /// <summary>
+        /// Person Name
+        /// </summary>
+        public string PersonName { get; set; }
+
+        /// <summary>
+        /// Person Game
+        /// </summary>
+        public string PersonGame { get; set; }
+    }
+}
